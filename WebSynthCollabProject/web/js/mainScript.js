71ac6c0e6bafb97a11fe9599c3a20868dfe8be16
Adrian O'Connor <adrian@adrianoc.com>
2016-02-01 15:35:05 +0000

    GUI Changes. Envelope implementation started.
    LFO changed to instance of Oscillator object. Waveform select dropdown
    added. On screen keyboard added (not yet functional). GUI controls and
    some code for envelope controls are present but not yet fully
    implemented.

--- WebSynthCollabProject/web/js/mainScript.js
+++ WebSynthCollabProject/web/js/mainScript.js
@@ -10,40 +10,72 @@ var rangeFactor = Math.pow ( 2, bitDepth - 1 );
 
 var webAudioContext = new AudioContext();
 
-var noteA4Frequency = 440;
-
-window.numSynths = 10;
-
+var noteA4Frequency = 440; // value in hz
+window.numSynths = 90;
 window.masterVolSetting = 25;
+window.onScreenKeyboardOctave = 0;
+
+window.oscillatorTypes = [ 'sine', 'square', 'sawtooth', 'triangle' ];
+
+var oscillatorSelectData = [
+    {
+        text: "Sine",
+        value: "sine",
+        description: "Sine Wave",
+        imageSrc: "./img/sine.png"
+    },
+    {
+        text: "Square",
+        value: "square",
+        description: "Square Wave",
+        imageSrc: "./img/square.png"
+    },
+    {
+        text: "Sawtooth",
+        value: "sawtooth",
+        description: "Sawtooth Wave",
+        imageSrc: "./img/sawtooth.png"
+    },
+    {
+        text: "Triangle",
+        value: "triangle",
+        description: "Triangle Wave",
+        imageSrc: "./img/triangle.png"
+    }
+];
+
+var expVals = [ 0.04, 0.08, 0.14, 0.21, 0.30, 0.41, 0.54, 0.71, 0.90, 1.14, 1.42, 1.76, 2.18, 2.69, 3.30, 4.05, 4.97, 6.09, 7.48, 9.18, 11.29, 13.91, 17.17, 21.23, 26.30, 32.66, 40.65, 50.72, 63.43, 79.51, 99.92, 125.87, 158.95, 201.21, 255.30, 324.70, 413.92, 528.86, 677.24, 869.17, 1117.93, 1440.95, 1861.21, 2408.99, 3124.30, 4060.07, 5286.40, 6896.33, 9013.48, 11802.36 ];
+
+var onScreenKeyboard;
 
 window.volSetting1 = 35;
 window.freqSetting1 = 440;
 window.waveformSetting1 = 'sine';
-window.AttackSetting1 = 1; // Attack value is in seconds.
+window.AttackSetting1 = 0.04; // Attack value is in seconds.
 window.DecaySetting1 = 0.1; // Decay value is in seconds.
 window.SustainSetting1 = 0.8;
-window.ReleaseSetting1 = 1; // Release value is in seconds.
-window.OctaveMultiplierValue1 = 1;
+window.ReleaseSetting1 = 0.14; // Release value is in seconds.
+window.OctaveMultiplierValue1 = 0;
 window.SemitoneOffsetValue1 = 0;
 
 window.volSetting2 = 35;
 window.freqSetting2 = 220;
 window.waveformSetting2 = 'square';
-window.AttackSetting2 = 1;
+window.AttackSetting2 = 0.04;
 window.DecaySetting2 = 0.1;
 window.SustainSetting2 = 0.8;
-window.ReleaseSetting2 = 1;
-window.OctaveMultiplierValue2 = 1;
+window.ReleaseSetting2 = 0.14;
+window.OctaveMultiplierValue2 = 0;
 window.SemitoneOffsetValue2 = 0;
 
 window.volSetting3 = 20;
 window.freqSetting3 = 660;
 window.waveformSetting3 = 'sawtooth';
-window.AttackSetting3 = 1;
+window.AttackSetting3 = 0.04;
 window.DecaySetting3 = 0.1;
 window.SustainSetting3 = 0.8;
-window.ReleaseSetting3 = 1;
-window.OctaveMultiplierValue3 = 1;
+window.ReleaseSetting3 = 0.14;
+window.OctaveMultiplierValue3 = 0;
 window.SemitoneOffsetValue3 = 0;
 
 window.LFOvol = 50;
@@ -105,7 +137,7 @@ Osc.prototype.changeVolume = function ( newVol ) {
 };
 
 Osc.prototype.changeWaveform = function ( newType ) {
-    this.oscillator.type = newFreq;
+    this.oscillator.type = newType;
 };
 
 Osc.prototype.changeAttack = function ( newAttack ) {
@@ -187,7 +219,7 @@ LFO.prototype.releaseNow = function () {
     
     this.envelopeNode.gain.exponentialRampToValueAtTime( 0.0001, currentTime + this.release );
 };
-
+        
 */
 
 function Synth () {
@@ -297,8 +329,20 @@ osc3VolNode.gain.value = volSetting3 / 100;
 
 $( document ).ready(function() {
 
-    // Create multiple synths for chords.
+    // Register on-screen keyboard.
 
+    onScreenKeyboard = document.getElementById('onScreenKeyboard');
+
+    onScreenKeyboard.addEventListener('noteon', function(e) {
+        console.log( "howiyagettinohnn: " + e.detail.index );
+    });
+
+    onScreenKeyboard.addEventListener('noteoff', function(e) {
+        console.log( "howiyagettinohff: " + e.detail.index );
+    });
+
+    // Create multiple synths for chords.
+    
     window.synths = [];
     
     window.synthStates = [];
@@ -306,47 +350,64 @@ $( document ).ready(function() {
     for ( var synthStateNo = 0; synthStateNo < numSynths; synthStateNo++ ) {
         window.synthStates[synthStateNo] = 128; // Use 128 for off. 0-127 denote note velocity.
     }
+    
+    var oscKnobWidth = 80;
+    var oscKnobThickness = 0.2;
 
     $("#master-volume-knob").knob({
                         'min':0,
                         'max':100,
                         'change' : function ( val ) { reviseMasterVolume( val ) },
+                        'width' : oscKnobWidth,
+                        'thickness' : oscKnobThickness,
                     });
 
     $("#volume-knob-1").knob({
                         'min':0,
                         'max':100,
                         'change' : function ( val ) { reviseVolume1( val ) },
+                        'width' : oscKnobWidth,
+                        'thickness' : oscKnobThickness,
                     });
                     
     $("#frequency-knob-1").knob({
                         'min':0,
                         'max':20000,
                         'change' : function ( val ) { reviseFreq1( val ) },
+                        'width' : oscKnobWidth,
+                        'thickness' : oscKnobThickness,
                     });
                     
     $("#volume-knob-2").knob({
                         'min':0,
                         'max':100,
                         'change' : function ( val ) { reviseVolume2( val ) },
+                        'width' : oscKnobWidth,
+                        'thickness' : oscKnobThickness,
                     });
                     
     $("#frequency-knob-2").knob({
                         'min':0,
                         'max':20000,
                         'change' : function ( val ) { reviseFreq2( val ) },
+                        'width' : oscKnobWidth,
+                        'thickness' : oscKnobThickness,
                     });
                     
     $("#volume-knob-3").knob({
                         'min':0,
                         'max':100,
                         'change' : function ( val ) { reviseVolume3( val ) },
+                        'width' : oscKnobWidth,
+                        'thickness' : oscKnobThickness,
                     });
                     
     $("#frequency-knob-3").knob({
                         'min':0,
                         'max':20000,
                         'change' : function ( val ) { reviseFreq3( val ) },
+                        'width' : oscKnobWidth,
+                        'thickness' : oscKnobThickness,
                     });
     
     $('#master-volume-knob')
@@ -377,6 +438,36 @@ $( document ).ready(function() {
         .val(window.freqSetting3)
         .trigger('change');
     
+    $('#waveformSelect1').empty();
+    
+    $('#waveformSelect1').ddslick({
+        data: oscillatorSelectData,
+        defaultSelectedIndex:0,
+        onSelected: function(data){
+            reviseWaveform1( data.selectedData.value );
+        }
+    });
+    
+    $('#waveformSelect2').empty();
+    
+    $('#waveformSelect2').ddslick({
+        data: oscillatorSelectData,
+        defaultSelectedIndex:1,
+        onSelected: function(data){
+            reviseWaveform2( data.selectedData.value );
+        }
+    });
+    
+    $('#waveformSelect3').empty();
+    
+    $('#waveformSelect3').ddslick({
+        data: oscillatorSelectData,
+        defaultSelectedIndex:2,
+        onSelected: function(data){
+            reviseWaveform3( data.selectedData.value );
+        }
+    });
+
 });
     
     
@@ -421,6 +512,8 @@ function processMidiInput ( midiMessage ) {
     if ( midiInfo[0] == 128 || ( midiInfo[0] == 144 && midiInfo[2] == 0 ) ) { // Note off message
         var note = midiInfo[1];
         
+        $("#onScreenKeyboard").find("div" + "[data-index='" + (note - 36 - ( window.onScreenKeyboardOctave * 12 ) ) + "']").removeClass("active");
+        
         for ( var curSynth = 0; curSynth < window.numSynths; curSynth++ ) {
             if ( window.synthStates[curSynth] == note ) {
                 window.synths[curSynth].setupReleaseCallback( curSynth );
@@ -445,15 +538,25 @@ function processMidiInput ( midiMessage ) {
                 // See https://en.wikipedia.org/wiki/MIDI_Tuning_Standard for info
                 // Semitone offset and octave multiplier must also be taken into account.
                 
+                var note1 = parseInt( note ) + ( parseInt(window.synths[curSynth].osc1.octaveMultiplier) * 12 ) + parseInt(window.synths[curSynth].osc1.semitoneOffset);
+                var note2 = parseInt( note ) + ( parseInt(window.synths[curSynth].osc2.octaveMultiplier) * 12 ) + parseInt(window.synths[curSynth].osc2.semitoneOffset);
+                var note3 = parseInt( note ) + ( parseInt(window.synths[curSynth].osc3.octaveMultiplier) * 12 ) + parseInt(window.synths[curSynth].osc3.semitoneOffset);
+                
                 window.synths[curSynth].osc1.changeFrequency (
-                    window.noteA4Frequency * Math.pow( 2, ( ( note - 69 + ( ( window.synths[curSynth].osc1.octaveMultiplier - 1 ) * 12 ) + window.synths[curSynth].osc1.semitoneOffset ) / 12 ) )    );
+                    window.noteA4Frequency * Math.pow( 2, ( ( note1 - 69 ) / 12 ) ) );
                 window.synths[curSynth].osc2.changeFrequency (
-                    window.noteA4Frequency * Math.pow( 2, ( ( note - 69 + ( ( window.synths[curSynth].osc2.octaveMultiplier - 1 ) * 12 ) + window.synths[curSynth].osc2.semitoneOffset ) / 12 ) )    );
+                    window.noteA4Frequency * Math.pow( 2, ( ( note2 - 69 ) / 12 ) ) );
                 window.synths[curSynth].osc3.changeFrequency (
-                    window.noteA4Frequency * Math.pow( 2, ( ( note - 69 + ( ( window.synths[curSynth].osc3.octaveMultiplier - 1 ) * 12 ) + window.synths[curSynth].osc3.semitoneOffset ) / 12 ) )    );
+                    window.noteA4Frequency * Math.pow( 2, ( ( note3 - 69 ) / 12 ) ) );
+                
+                console.log( "Osc1 freq: " + ( window.noteA4Frequency * Math.pow( 2, ( ( note1 - 69 ) / 12 ) ) ) );
+                console.log( "Osc2 freq: " + ( window.noteA4Frequency * Math.pow( 2, ( ( note2 - 69 ) / 12 ) ) ) );
+                console.log( "Osc3 freq: " + ( window.noteA4Frequency * Math.pow( 2, ( ( note3 - 69 ) / 12 ) ) ) );
                 
                 window.synths[curSynth].connect();
                 window.synths[curSynth].start();
+                
+                $("#onScreenKeyboard").find("div" + "[data-index='" + ( note - 36 - ( window.onScreenKeyboardOctave * 12 ) ) + "']").addClass("active");
                 
                 break;
             }
@@ -543,6 +646,12 @@ function MasterVolumeTextChange () {
     reviseMasterVolume( masterVolSetting );
 }
 
+function changeOnScreenKeyboardOctaveSelect () {
+    var onScreenKeyboardOctaveSelect = document.getElementById("onScreenKeyboardOctaveSelect");
+    var newOctave = onScreenKeyboardOctaveSelect.options[onScreenKeyboardOctaveSelect.selectedIndex].value;
+    reviseOnScreenKeyboardOctave( newOctave );
+}
+
 function VolumeTextChange1 () {
     var newVolSetting = document.getElementById("volume-knob-1").value;
     reviseVolume1( newVolSetting );
@@ -553,19 +662,27 @@ function FreqTextChange1() {
     reviseFreq1( newFreqSetting );
 }
 
+function changeOsc1WaveformSelect() {
+    var osc1WaveformSelect = document.getElementById("waveformSelect1");
+    var newType = osc1WaveformSelect.options[osc1WaveformSelect.selectedIndex].value;
+    reviseWaveform1( newType );
+}
+
+function changeOsc1OctaveSelect() {
+    var osc1OctaveSelect = document.getElementById("octaveSelect1");
+    var newOctave = osc1OctaveSelect.options[osc1OctaveSelect.selectedIndex].value;
+    reviseOctave1( newOctave );
+}
+
+function changeOsc1SemitoneSelect() {
+    var osc1SemitoneSelect = document.getElementById("semitoneSelect1");
+    var newVal = osc1SemitoneSelect.options[osc1SemitoneSelect.selectedIndex].value;
+    reviseSemitoneOffset1( newVal );
+}
+
 function VolumeTextChange2 () {
     var newVolSetting = document.getElementById("volume-knob-2").value;
     reviseVolume2( newVolSetting );
-}
-
-function FreqTextChange3() {
-    var newFreqSetting = document.getElementById("frequency-knob-3").value;
-    reviseFreq3( newFreqSetting );
-}
-
-function VolumeTextChange3 () {
-    var newVolSetting = document.getElementById("volume-knob-3").value;
-    reviseVolume3( newVolSetting );
 }
 
 function FreqTextChange2() {
@@ -573,9 +690,59 @@ function FreqTextChange2() {
     reviseFreq2( newFreqSetting );
 }
 
+function changeOsc2WaveformSelect() {
+    var osc2WaveformSelect = document.getElementById("waveformSelect2");
+    var newType = osc2WaveformSelect.options[osc2WaveformSelect.selectedIndex].value;
+    reviseWaveform2( newType );
+}
+
+function changeOsc2OctaveSelect() {
+    var osc2OctaveSelect = document.getElementById("octaveSelect2");
+    var newOctave = osc2OctaveSelect.options[osc2OctaveSelect.selectedIndex].value;
+    reviseOctave2( newOctave );
+}
+
+function changeOsc2SemitoneSelect() {
+    var osc2SemitoneSelect = document.getElementById("semitoneSelect2");
+    var newVal = osc2SemitoneSelect.options[osc2SemitoneSelect.selectedIndex].value;
+    reviseSemitoneOffset2( newVal );
+}
+
+function VolumeTextChange3 () {
+    var newVolSetting = document.getElementById("volume-knob-3").value;
+    reviseVolume3( newVolSetting );
+}
+
+function FreqTextChange3() {
+    var newFreqSetting = document.getElementById("frequency-knob-3").value;
+    reviseFreq3( newFreqSetting );
+}
+
+function changeOsc3WaveformSelect() {
+    var osc3WaveformSelect = document.getElementById("waveformSelect3");
+    var newType = osc3WaveformSelect.options[osc3WaveformSelect.selectedIndex].value;
+    reviseWaveform3( newType );
+}
+
+function changeOsc3OctaveSelect() {
+    var osc3OctaveSelect = document.getElementById("octaveSelect3");
+    var newOctave = osc3OctaveSelect.options[osc3OctaveSelect.selectedIndex].value;
+    reviseOctave3( newOctave );
+}
+
+function changeOsc3SemitoneSelect() {
+    var osc3SemitoneSelect = document.getElementById("semitoneSelect3");
+    var newVal = osc3SemitoneSelect.options[osc3SemitoneSelect.selectedIndex].value;
+    reviseSemitoneOffset3( newVal );
+}
+
 function reviseMasterVolume ( vol ) {
     window.masterVolSetting = vol;
     volumeNode.gain.value = vol / 100;
+}
+
+function reviseOnScreenKeyboardOctave ( newOctave ) {
+    window.onScreenKeyboardOctave = newOctave;
 }
 
 function reviseVolume1 ( vol ) {
@@ -588,6 +755,18 @@ function reviseFreq1 ( freq ) {
     osc1.frequency.value = freq;
 }
 
+function reviseWaveform1 ( type ) {
+    window.waveformSetting1 = type;
+}
+
+function reviseOctave1 ( newOctave ) {
+    window.OctaveMultiplierValue1 = newOctave;
+}
+
+function reviseSemitoneOffset1 ( newVal ) {
+    window.SemitoneOffsetValue1 = newVal;
+}
+
 function reviseVolume2 ( vol ) {
     window.volSetting2 = vol;
     osc2VolNode.gain.value = vol / 100;
@@ -598,6 +777,18 @@ function reviseFreq2 ( freq ) {
     osc2.frequency.value = freq;
 }
 
+function reviseWaveform2 ( type ) {
+    window.waveformSetting2 = type;
+}
+
+function reviseOctave2 ( newOctave ) {
+    window.OctaveMultiplierValue2 = newOctave;
+}
+
+function reviseSemitoneOffset2 ( newVal ) {
+    window.SemitoneOffsetValue2 = newVal;
+}
+
 function reviseVolume3 ( vol ) {
     window.volSetting3 = vol;
     osc3VolNode.gain.value = vol / 100;
@@ -606,4 +797,96 @@ function reviseVolume3 ( vol ) {
 function reviseFreq3 ( freq ) {
     window.freqSetting3 = freq;
     osc3.frequency.value = freq;
+}
+
+function reviseWaveform3 ( type ) {
+    window.waveformSetting3 = type;
+}
+
+function reviseOctave3 ( newOctave ) {
+    window.OctaveMultiplierValue3 = newOctave;
+}
+
+function reviseSemitoneOffset3 ( newVal ) {
+    window.SemitoneOffsetValue3 = newVal;
+}
+
+function reviseAttack3 ( newMsVal ) {
+    window.AttackSetting3 = ( newMsVal / 1000 );
+}
+
+function updateAttackText3 ( ) {
+    var attackSelect3 = document.getElementById("attackSelect3");
+    var attackDisplay3 = document.getElementById("attackDisplay3");
+    
+    var newLinVal = attackSelect3.value;
+    var newVal = Math.pow(newLinVal, newLinVal) - 0.7;
+    
+    attackDisplay3.value = newVal.toFixed(2);
+    
+    reviseAttack3( attackDisplay3.value );
+}
+
+function updateAttackSlider3 ( ) {
+    var attackDisplay3 = document.getElementById("attackDisplay3");
+    var attackSelect3 = document.getElementById("attackSelect3");
+    
+    var newVal = attackDisplay3.value;
+    
+    var max = true;
+    var curIndex = 0;
+    for ( var curVal = 0.6; curVal < 5.5; curVal+= 0.1 ) {
+        if ( window.expVals[curIndex] >= newVal ) {
+            attackSelect3.value = curVal;
+            max = false;
+            break;
+        }
+        curIndex++;
+    }
+    
+    if ( max ) {
+        attackSelect3.value = 5.5;
+    }
+    
+    reviseAttack3( attackDisplay3.value );
+}
+
+function reviseRelease3 ( newMsVal ) {
+    window.ReleaseSetting3 = ( newMsVal / 1000 );
+}
+
+function updateReleaseText3 ( ) {
+    var releaseSelect3 = document.getElementById("releaseSelect3");
+    var releaseDisplay3 = document.getElementById("releaseDisplay3");
+    
+    var newLinVal = releaseSelect3.value;
+    var newVal = Math.pow(newLinVal, newLinVal) - 0.7;
+    
+    releaseDisplay3.value = newVal.toFixed(2);
+    
+    reviseAttack3( releaseDisplay3.value );
+}
+
+function updateReleaseSlider3 ( ) {
+    var releaseDisplay3 = document.getElementById("releaseDisplay3");
+    var releaseSelect3 = document.getElementById("releaseSelect3");
+    
+    var newVal = releaseDisplay3.value;
+    
+    var max = true;
+    var curIndex = 0;
+    for ( var curVal = 0.6; curVal < 5.5; curVal+= 0.1 ) {
+        if ( window.expVals[curIndex] >= newVal ) {
+            releaseSelect3.value = curVal;
+            max = false;
+            break;
+        }
+        curIndex++;
+    }
+    
+    if ( max ) {
+        releaseSelect3.value = 5.5;
+    }
+    
+    reviseRelease3( releaseDisplay3.value );
 }
